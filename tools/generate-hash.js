2206e21fd733aa292f7b5d763c4303946ca4919b
Max <max.liao9329@gmail.com>
2025-05-26 01:04:13 +0800

    管理者登入

--- tools/generate-hash.js
+++ tools/generate-hash.js
@@ -4,7 +4,7 @@
 import bcrypt from 'bcrypt';
 
 const run = async () => {
-  const password = 'a12345678'; // ← 請改成你想加密的密碼
+  const password = 'admin1234'; // ← 請改成你想加密的密碼
   const hash = await bcrypt.hash(password, 10);
   console.log(`✅ bcrypt hash for "${password}":\n${hash}`);
 };
